513216302506277ed3841d6b580c9a5b7db3ea67
Alisa Harodnik <alisa.solovchuk@mail.ru>
2021-05-26 17:30:52 +0300

    Homework 14 has done

--- src/test/java/tests/BaseTest.java
+++ src/test/java/tests/BaseTest.java
@@ -5,10 +5,12 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
-import pages.*;
+import org.testng.annotations.Listeners;
+import pages.CartPage;
+import pages.LoginPage;
+import pages.ProductsPage;
 
-import java.util.concurrent.TimeUnit;
-
+@Listeners(TestListener.class)
 public class BaseTest {
     WebDriver driver;
     ProductsPage productsPage;
@@ -24,7 +26,6 @@ public class BaseTest {
         WebDriverManager.chromedriver().setup();
         driver = new ChromeDriver();
         driver.manage().window().maximize();
-        driver.manage().timeouts().implicitlyWait(20, TimeUnit.SECONDS);
         initPages();
     }
 
