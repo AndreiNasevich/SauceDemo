a0025a553c61a76c67455875d8195799fc52d5ee
Alisa Harodnik <alisa.solovchuk@mail.ru>
2021-06-11 20:00:16 +0300

    add config.properties and PropertyReader.java

--- src/test/java/constants/ITestConstants.java
+++ src/test/java/constants/ITestConstants.java
@@ -1,9 +1,7 @@
 package constants;
 
 public interface ITestConstants {
-    String STANDARD_USER_LOGIN = "standard_user";
     String EMPTY_STRING = "";
-    String STANDARD_PASSWORD = "secret_sauce";
     String SAUCE_LABS_BACKPACK_PRODUCT = "Sauce Labs Backpack";
     String SAUCE_LABS_BOLT_T_SHIRT = "Sauce Labs Bolt T-Shirt";
     String EXPECTED_REMOVE_BUTTON_NAME = "REMOVE";
